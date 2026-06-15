e5e1e47e7a7fb4c8b18fe3296fbfa49e8b145a12
AsifShahriar250 <cse12305029brur@gmail.com>
2026-06-16 04:50:24 +0600

    new file

---
+++ moon.js
@@ -0,0 +1,6 @@
+function showMenu(id, btn) {
+  document.querySelectorAll('.menu-panel').forEach(p => p.classList.remove('active'));
+  document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+  btn.classList.add('active');
+}
